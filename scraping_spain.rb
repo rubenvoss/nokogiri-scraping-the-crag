9d904dec4d348ba9f8a82d4bcdb427e759cb2999
Ruben Voss <bekertshop@gmail.com>
2022-09-05 22:34:37 +0200

    working on scraping spain

--- scraping_spain.rb
+++ scraping_spain.rb
@@ -2,5 +2,13 @@ require "nokogiri"
 require "open-uri"
 require "pry"
 
-binding.pry
-doc = Nokogiri::HTML(open("https://www.thecrag.com/de/klettern/spain/barcelona-area"))
+doc = Nokogiri::HTML(open("./Catalan coastal ranges north, Sportklettern _ theCrag.html"))
+
+doc = doc.css("div.node-listview.hide-archived")
+# doc = doc.css("div.area")[0]
+# doc = doc.css("div.name")
+doc = doc.css("span.primary-node-name")
+# doc = doc.css("span.type")
+
+
+p doc
