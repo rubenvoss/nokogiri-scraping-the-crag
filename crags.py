cd743401bb64da6be0ecd81451380ba6d1c4904f
Ruben Voss <bekertshop@gmail.com>
2022-09-06 00:34:01 +0200

    getting somewhere..

--- crags.py
+++ crags.py
@@ -1,5 +1,10 @@
+from unicodedata import name
 from bs4 import BeautifulSoup
 import requests
+import pdb
+# import mymodule
+# import numpy as np
+
 
 
 # use this if you want to parse from live website
@@ -20,16 +25,22 @@ area_divs = soup.find_all("div", class_ = "area")
 
 for div in area_divs:
     div = div.find("span", class_ = "primary-node-name")
-
-
     if div is None:
         pass
     else:
         crag_name = div.text
         print(crag_name)
 
-        a = div.find("div", class_ = "name")
-        print(a['href'])
+        name_div = div.find("div", class_ = "name")
+        pdb.run('mymodule.test()')
+        if name_div is None:
+            pass
+        else:
+            url = name_div.find("a")["href"]
+            print(url)
+
+
+
 
 
 # with open('crags.html', 'r') as html_file:
